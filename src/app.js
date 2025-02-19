0edc488f18e327ae37b72a58d7456b4c6733ddee
Pedro <114964122+pedrodeleondev@users.noreply.github.com>
2025-02-19 08:13:07 -0600

    Dev-Pedro-Implementcion-Se hizo la implementacion de mysql

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
+const mysql = require('mysql2');
 const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// string de conexion a cluster de MongoDB
-mongoose.connect('mongodb+srv://admin:1234@test.bnnzmy9.mongodb.net/?retryWrites=true&w=majority&appName=Test')
-  .then(() => console.log('MongoDB conectado'))
-  .catch(err => console.log('Error conectando a MongoDB:', err));
+// Conexión a MySQL
+const db = mysql.createConnection({
+    host: process.env.DB_HOST || 'bbz7w1sdy5yaf7ocvlva-mysql.services.clever-cloud.com',
+    user: process.env.DB_USER || 'ufvomre2iqltqutc',
+    password: process.env.DB_PASSWORD || 'JfEy8nMBIVlNIQcuASay',
+    database: process.env.DB_NAME || 'bbz7w1sdy5yaf7ocvlva',
+    port: process.env.DB_PORT || 3306
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Error conectando a MySQL:', err);
+        return;
+    }
+    console.log('Conectado a MySQL');
+});
 
 // Importar rutas
 const authRoutes = require('./routes/authRoutes');
@@ -19,12 +32,11 @@ const productRoutes = require('./routes/productRoutes');
 app.use('/auth', authRoutes);
 app.use('/products', productRoutes);
 
-
 // Servir archivos estáticos desde la carpeta public
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en: ${PORT}`);
+    console.log(`Servidor corriendo en: ${PORT}`);
 });
 
 module.exports = app;
